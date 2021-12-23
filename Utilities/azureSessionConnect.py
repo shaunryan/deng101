0826cd896fd7d02543ede2a167594556bd42f4ca
shaunryan <shaunryan>
2021-12-23 16:25:44 +0000

    demo

--- Utilities/azureSessionConnect.py
+++ Utilities/azureSessionConnect.py
@@ -1,12 +1,4 @@
 # Databricks notebook source
-from utilities import test
-
-# test.run()
-
-
-
-# COMMAND ----------
-
 from utilities import AppConfig
 
 app_config = AppConfig(dbutils, spark)
--- Utilities/utilities/__init__.py
+++
@@ -1,4 +0,0 @@
-from .config import AppConfig
-
-__all__ = ["AppConfig"]
-
