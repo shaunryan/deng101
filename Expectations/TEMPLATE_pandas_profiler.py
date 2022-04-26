b48dbb8e098bb19358c38eb41c38063fc30b4428
shaun-ryan <82893655+shaun-ryan@users.noreply.github.com>
2022-04-26 09:35:52 +0100

    Create TEMPLATE_pandas_profiler.py

---
+++ Expectations/TEMPLATE_pandas_profiler.py
@@ -0,0 +1,51 @@
+# Databricks notebook source
+# MAGIC %pip install pandas-profiling
+
+# COMMAND ----------
+
+from pyspark.sql.functions import expr
+import pandas as pd
+from pandas_profiling import ProfileReport
+
+
+# COMMAND ----------
+
+import pandas as pd
+from pandas_profiling import ProfileReport
+
+
+################################ Customise ######################################################
+# from pyspark.sql.types import *
+
+# schema = schema = StructType([ \
+#     StructField('ID' , IntegerType(),True), \
+#     ?
+#   ])
+
+
+# df = (spark
+#       .read
+#       .format("csv")
+#       .schema(schema)
+#       .option("mode", "failfast")
+#       .load("?"))
+############################################################################################
+
+display(df)
+
+
+# COMMAND ----------
+
+profile = ProfileReport(df.toPandas(), title="Pandas Profiling Report", explorative=True)
+profile.to_file('/tmp/profile_report.html')
+dbutils.fs.ls("file:/tmp/")
+dbutils.fs.cp("file:/tmp/profile_report.html", "/FileStore/data_profiles/profile_report.html")
+displayHTML("""
+<a href='https://adb-1593923126743168.8.azuredatabricks.net/files/data_profiles/profile_report.html'>report</a>
+""")
+
+
+
+# COMMAND ----------
+
+displayHTML(profile.to_html())
